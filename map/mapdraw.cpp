cb30e85a7e5db62ceb9ae0d165f0b2e306add036
Xiaoxiao <15779212418@qq.com>
2024-07-26 13:59:48 +0800

    框架

---
+++ map/mapdraw.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 15779 on 24-7-26.
+//
+
+#include "mapdraw.h"
---
+++ map/mapdraw.h
@@ -0,0 +1,14 @@
+//
+// Created by 15779 on 24-7-26.
+//
+
+#ifndef RICH_MAN_MAPDRAW_H
+#define RICH_MAN_MAPDRAW_H
+
+
+class mapdraw {
+
+};
+
+
+#endif //RICH_MAN_MAPDRAW_H
