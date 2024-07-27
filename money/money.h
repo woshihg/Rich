05de67c751200c6b8c5e0733f3ae5197da8a8413
Riverise <2050259248@qq.com>
2024-07-27 13:12:19 +0800

    添加买房、升级房的功能

--- money/money.h
+++ money/money.h
@@ -4,5 +4,15 @@
 
 #ifndef MONEY_H
 #define MONEY_H
+//
+// Created by Lenovo on 24-7-26.
+// Created by 陈曦 on 2024/7/27.
+//
+
+#include "../json/json.h"
+
+void invest_house_logit(Player* player ,Cell* cell);
+void invest_house_execute(Player* player, Cell* cell);
+int get_cost(int rank);
 
 #endif //MONEY_H
